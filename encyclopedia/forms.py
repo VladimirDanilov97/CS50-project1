f4ef5f40d0791736811bcbb4d5ea35cdc4650133
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-01-08 12:00:08 +0300

    add some new bootstrap

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from django.forms.widgets import TextInput, Textarea
 
 class CreateNewPage(forms.Form):
-    title = forms.CharField(max_length=120, min_length=1, widget=TextInput(attrs={'class':'titleclass'}))
+    title = forms.CharField(max_length=120, min_length=1, widget=TextInput(attrs={'class':'input-group-text'}))
     content = forms.CharField(widget=Textarea(attrs={'class':'textclass'}))

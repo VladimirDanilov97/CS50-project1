6c8e0158c3533726d74fa56c9ea82b986ad8f5b0
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-01-11 10:11:29 +0300

    css

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -3,4 +3,4 @@ from django.forms.widgets import TextInput, Textarea
 
 class CreateNewPage(forms.Form):
     title = forms.CharField(max_length=120, min_length=1, widget=TextInput(attrs={'class':'input-group-text'}))
-    content = forms.CharField(widget=Textarea(attrs={'class':'textclass'}))
+    content = forms.CharField(widget=Textarea(attrs={'class':'input-group-text'}))
